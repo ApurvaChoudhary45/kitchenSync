fe9bea9f9e8a0eee7e3357d45026dbe0ad005f1a
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-06-11 22:48:43 +0530

    ready

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -4,7 +4,6 @@ const URI = process.env.MONGO_URI
 const client = new MongoClient(URI)
 export async function GET(request) {
     await client.connect()
-    const body = await request.body;
     const order = client.db('Apurv')
     const carted = order.collection('Cart')
     const result = await carted.find({}).toArray()
