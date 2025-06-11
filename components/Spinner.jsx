ac72b781cef24d02f8ddc0c45f0f12266af34265
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-06-11 22:11:22 +0530

    fixes

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -4,7 +4,7 @@ const Spinner = () => {
   return (
     <div>
       <div className="col-span-3 flex justify-center items-center py-20 w-full">
-        <div className="w-16 h-16 border-4 border-green-500 border-t-transparent rounded-full animate-spin text-center">
+        <div className="w-18 h-16 border-4 border-green-500 border-t-transparent rounded-full animate-spin text-center">
         </div>
       </div>
 
