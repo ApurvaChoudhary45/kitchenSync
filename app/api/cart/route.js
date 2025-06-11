8aaebde3e4f1989a3f7b1954bb21cb2494ed5500
ApurvaChoudhary45 <badassmannu45@gmail.com>
2025-06-11 17:51:31 +0530

    few fixess

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -4,6 +4,7 @@ const URI = process.env.MONGO_URI
 const client = new MongoClient(URI)
 export async function GET(request) {
     await client.connect()
+    const body = await request.body;
     const order = client.db('Apurv')
     const carted = order.collection('Cart')
     const result = await carted.find({}).toArray()
--- app/api/history/route.js
+++ app/api/history/route.js
@@ -4,6 +4,7 @@ const URI = process.env.MONGO_URI
 const client = new MongoClient(URI)
 export async function GET(request) {
     await client.connect()
+    const body = await request.body;
     const orders = client.db('Apurv')
     const placed = orders.collection('Placed Order')
     const results = await placed.find({}).toArray()
